fdeefe8f81c20d4e8674b057a9f7a9bf8828e6a9
Janus Developer <dev@retrievo.net>
2024-08-17 07:35:40 -0400

    add record detail view

--- website/urls.py
+++ website/urls.py
@@ -8,4 +8,6 @@ app_name = "website"
 urlpatterns = [
     path("", views.home, name="home"),
     path("logout/", views.logout_user, name="logout"),
+    path("register/", views.register_user, name="register"),
+    path("records/<int:pk>/", views.record_detail, name="record"),
 ]
--- website/views.py
+++ website/views.py
@@ -2,10 +2,14 @@ from django.shortcuts import redirect, render
 from django.conf import settings
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from .forms import SignUpForm
+from .models import Record
 
 
 # Create your views here.
 def home(request):
+    records = Record.objects.all()
+
     if request.method == "POST":
         username = request.POST["username"]
         password = request.POST["password"]
@@ -21,8 +25,39 @@ def home(request):
             )
             return redirect("website:home")
 
-    return render(request, "website/home.html", {})
+    return render(request, "website/home.html", {"records": records})
 
 
 def logout_user(request):
-    pass
+    logout(request)
+    messages.success(request, "You have been logged out.")
+    return redirect("website:home")
+
+
+def register_user(request):
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # Authenticate and login
+            username = form.cleaned_data["username"]
+            password = form.cleaned_data["password1"]
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.success(request, "You have successfully registered")
+                return redirect("website:home")
+    else:
+        form = SignUpForm()
+        return render(request, "website/register.html", {"form": form})
+
+    return render(request, "website/register.html", {"form": form})
+
+
+def record_detail(request, pk):
+    if request.user.is_authenticated:
+        record = Record.objects.get(id=pk)
+        return render(request, "website/record.html", {"record": record})
+    else:
+        messages.success(request, "You must be logged in to view this page.")
+    return redirect("website:home")
